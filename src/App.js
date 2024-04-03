252697a3b570d6dea78b7abd5f1f95a7e7c35d1d
Aviral000 <avimalik02@gmail.com>
2024-04-03 18:26:38 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Albumlist from './components/Albumlist';
 function App() {
   return (
     <div className="App">
+      <h1 style={{ marginLeft: "2rem", color: "gray" }}>Album List:</h1>
       <Albumlist />
     </div>
   );
